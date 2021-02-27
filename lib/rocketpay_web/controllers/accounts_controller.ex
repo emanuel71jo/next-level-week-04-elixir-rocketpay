73cbcce174329899d2628e2d9a318a782c286961
emanuel71jo <emanuel71jo@gmail.com>
2021-02-27 08:30:10 -0300

    Quarto dia do nlw 4, trilha elixir

--- lib/rocketpay_web/controllers/accounts_controller.ex
+++ lib/rocketpay_web/controllers/accounts_controller.ex
@@ -2,6 +2,8 @@ defmodule RocketpayWeb.AccountsController do
   use RocketpayWeb, :controller
 
   alias Rocketpay.Account
+  alias Rocketpay.Accounts.Transactions.Response, as: TransactionResponse
+
 
   action_fallback RocketpayWeb.FallbackController
 
@@ -14,6 +16,18 @@ defmodule RocketpayWeb.AccountsController do
   end
 
   def withdraw(conn, params) do
+    with {:ok, %Account{} = account} <- Rocketpay.withdraw(params) do
+      conn
+      |> put_status(:create)
+      |> render("update.json", account: account)
+    end
+  end
 
+  def transaction(conn, params) do
+    with {:ok, %TransactionResponse{} = transaction} <- Rocketpay.transaction(params) do
+      conn
+      |> put_status(:create)
+      |> render("transaction.json", transaction: transaction)
+    end
   end
 end
